da7e0367874d15e59e8cbbedc2a80c5e4137ead2
Sayan Dey <111982005+Sayan-97@users.noreply.github.com>
2023-03-21 01:31:59 +0530

    added changes

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById("goback_btn").onclick = function () {
+    location.href = "./index.html";
+};
+
+document.getElementById("galleryBtn").onclick = function () {
+    location.href = "./gallery.html";
+};
+
